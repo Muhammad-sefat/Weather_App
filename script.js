28bbdf5074fd8ffdc50d046612dbc2a4e77976d2
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-12-17 08:20:21 +0600

    complete css and html part and showing city name and country name

--- script.js
+++ script.js
@@ -18,46 +18,45 @@ const getCountryName = (code) => {
   return new Intl.DisplayNames([code], { type: "region" }).of(code);
 };
 
-// to get the date and time
-const getDateTime = (dt) => {
-  const curDate = new Date(dt * 1000); // Convert seconds to milliseconds
-  console.log(curDate);
-  // // const date = new Date();
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-  };
+// // to get the date and time
+// const getDateTime = (dt) => {
+//   const curDate = new Date(dt * 1000); // Convert seconds to milliseconds
+//   console.log(curDate);
+//   // // const date = new Date();
+//   const options = {
+//     weekday: "long",
+//     year: "numeric",
+//     month: "long",
+//     day: "numeric",
+//     hour: "numeric",
+//     minute: "numeric",
+//   };
 
-  const formatter = new Intl.DateTimeFormat("en-US", options);
-  console.log(formatter);
-  return formatter.format(curDate);
-};
+//   const formatter = new Intl.DateTimeFormat("en-US", options);
+//   console.log(formatter);
+//   return formatter.format(curDate);
+// };
 
-let city = "pune";
+// let city = "Dhaka";
 
-// search functionality
-citySearch.addEventListener("submit", (e) => {
-  e.preventDefault();
+// // search functionality
+// citySearch.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  let cityName = document.querySelector(".city_name");
-  console.log(cityName.value);
-  city = cityName.value;
+//   let cityName = document.querySelector(".city_name");
+//   console.log(cityName.value);
+//   city = cityName.value;
 
-  getWeatherData();
+//   getWeatherData();
 
-  cityName.value = "";
-});
+//   cityName.value = "";
+// });
 
 const getWeatherData = async () => {
-  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=addYourOwnAPIKey`;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=d3ba1bc1b727ac36376bdb50bbc19249`;
   try {
     const res = await fetch(weatherUrl);
     const data = await res.json();
-    console.log(data);
 
     const { main, name, weather, wind, sys, dt } = data;
 
